ce6d342e3955c97c912f84bf33bd529a8405ace0
Metod Jazbec <mjazbec@ivi-cn002.ivi.local>
2023-02-22 11:26:51 +0100

    marginal likelihood further investigation

--- examples/helper/calibration_gp_utils.py
+++ examples/helper/calibration_gp_utils.py
@@ -30,7 +30,7 @@ def predict(dataloader, model, laplace=False):
 
 def gp_calibration_eval(model, train_loader, test_loader, subset_of_weights='last_layer',
                         M_arr=[10, 50, 100, 300, 500, 1000], prior_precision=1.0,
-                        optimize_prior_precision=True, n_seeds=1, opt_type='marglik') -> pd.DataFrame:
+                        optimize_prior_precision=True, n_seeds=3, opt_type='marglik') -> pd.DataFrame:
     targets = torch.cat([y for x, y in test_loader], dim=0).cpu()
     metrics_df = pd.DataFrame()
     for m in M_arr:
@@ -43,7 +43,7 @@ def gp_calibration_eval(model, train_loader, test_loader, subset_of_weights='las
             la.fit(train_loader)
             if optimize_prior_precision:
                 la.optimize_prior_precision(method=opt_type, val_loader=test_loader,
-                                            init_prior_prec=prior_precision)
+                                            init_prior_prec=prior_precision, verbose=True)
 
             probs_laplace = predict(test_loader, la, laplace=True)
             acc_laplace = (probs_laplace.argmax(-1) == targets).float().mean()
